f19c09f67bff3bd656f125b796ee96506306e50c
unknown <markx195>
2021-12-11 00:10:49 +0700

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BoxMainpageComponent } from './boxMainpage/box-mainpage.component';
+import { DetailComponent } from './detail/detail.component';
+
 
 const routes: Routes = [
-  {path:'', component: BoxMainpageComponent}
+  {path:'', component: BoxMainpageComponent},
+  {path: 'detail', component: DetailComponent},
 ];
 
 @NgModule({
